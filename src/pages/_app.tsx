1cf39ddf351a1759282de7b43c52caa72bd93593
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-24 18:48:25 -0300

    Container added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import Layout from '@/layouts/layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
+import styles from '../styles/Home.module.css'
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
       <Layout>
-        <Component {...pageProps} />
+        <div className={styles.container}>
+          <Component {...pageProps} />
+        </div>
       </Layout>
     </div>
   )
