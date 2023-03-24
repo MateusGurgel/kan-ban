d19a6585364721028b6e2ffb501c3d9f08cbed68
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-24 18:14:41 -0300

    variable font added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import Layout from '@/layouts/layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div className={inter.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   )
 }
