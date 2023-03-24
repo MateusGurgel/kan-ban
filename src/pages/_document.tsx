ae999e6a2cac25a50f535972ce98d40dfbb3597a
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-24 18:14:31 -0300

    font removed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head> 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&display=swap" rel="stylesheet" />
-      </Head>
+      <Head/> 
       <body>
         <Main />
         <NextScript />
