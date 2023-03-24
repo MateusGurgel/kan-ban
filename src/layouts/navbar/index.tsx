edd73afec37db3b45b5cb3e147e26584c62c00b3
Mateus <mateusgurgel15@gmail.com>
2023-03-24 12:38:00 -0300

    nav added

--- src/layouts/navbar/index.tsx
+++ src/layouts/navbar/index.tsx
@@ -4,6 +4,15 @@ import styles from "./styles.module.css"
 export function NavBar() {
     return (
     <header className={styles.header}>
-        <h1>Header</h1>
+        <section className={styles.content}>    
+            <h1>Header</h1>
+
+            <nav>
+                <h1>Header</h1>
+                <h1>Header</h1>
+
+            </nav>
+
+        </section>
     </header>)
 }
