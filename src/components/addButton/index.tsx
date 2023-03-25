ba92a208806287c92c1603192c050fdec6b76534
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-25 18:47:05 -0300

    initial commit

---
+++ src/components/addButton/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.css";
+import { ReactNode } from "react";
+
+interface AddButtonProps {
+  children: ReactNode;
+  onClick?: () => void;
+  disabled?: boolean;
+}
+
+export default function AddButton(props: AddButtonProps) {
+  return (
+    <button 
+    onClick={props.onClick} 
+    disabled={props.disabled}
+    
+    >
+      {props.children}{" "}
+    </button>
+  );
+}
