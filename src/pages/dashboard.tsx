91dd93ed4780fbe73cc9194a9499ebe9cb28c3f5
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-25 15:57:41 -0300

    Dashboard added

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import {BiPlus} from "react-icons/bi"
+import styles from "../styles/Dashboard.module.css";
+
+export default function Login() {
+  function createKanBan() {}
+
+  return (
+    <div className={styles.dashboardContainer}>
+      <Head>
+        <title>Dashboard</title>
+        <meta
+          name="description"
+          content="Dashboard of the kan-ban! aplication"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.dashboardCard}>
+        <h1>my kanbans</h1>
+
+        <button className={styles.KanBanbutton}>Task</button>
+        <button className={styles.addButton}><BiPlus/></button>
+      </div>
+    </div>
+  );
+}
