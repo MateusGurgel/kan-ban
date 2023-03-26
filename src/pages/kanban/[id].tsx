3cd3b22357053341fa66b33639389e680420bda2
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-25 21:26:48 -0300

    KanBanPage finished

--- src/pages/kanban/[id].tsx
+++ src/pages/kanban/[id].tsx
@@ -1,7 +1,31 @@
-import styles from "./../../styles/KanBan.module.css"
+import AddButton from "@/components/addButton";
+import Head from "next/head";
+import styles from "./../../styles/KanBan.module.css";
 
-export default function kanban () {
-    return (
-        <h1>kanBan</h1>
-    )
+export default function kanban() {
+  return (
+    <div className={styles.kanban}>
+      <Head>
+        <title>Login</title>
+        <meta name="description" content="login of the kan-ban! aplication" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+     <div className={styles.list}>
+        <div className={styles.listHeader}>To do</div>
+        <AddButton/>
+
+     </div>
+
+     <div className={styles.list}>
+        <div className={styles.listHeader}>In progress</div>
+     </div>
+
+     <div className={styles.list}>
+        <div className={styles.listHeader}>Done</div>
+     </div>
+
+    </div>
+  );
 }
