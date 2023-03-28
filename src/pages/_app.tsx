8ac8ccbddd88b07e2db7891a21389b897157b7b4
Mateus <mateusgurgel15@gmail.com>
2023-03-28 09:56:35 -0300

    Animate presence added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,22 @@
-import Layout from '@/layouts/layout'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Inter } from 'next/font/google'
-import styles from '../styles/App.module.css'
+import Layout from "@/layouts/layout";
+import "@/styles/globals.css";
+import { AnimatePresence } from "framer-motion";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import styles from "../styles/App.module.css";
 
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={inter.className}>
-      <Layout>
-        <div className={styles.container}>
-          <Component {...pageProps} />
-        </div>
-      </Layout>
-    </div>
-  )
+    <AnimatePresence>
+      <div className={inter.className}>
+        <Layout>
+          <div className={styles.container}>
+            <Component {...pageProps} />
+          </div>
+        </Layout>
+      </div>
+    </AnimatePresence>
+  );
 }
