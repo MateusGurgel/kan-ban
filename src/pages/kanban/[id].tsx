a56334a38c53f641c884b9b609730e1385333155
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-28 17:31:26 -0300

    New mobile layout

--- src/pages/kanban/[id].tsx
+++ src/pages/kanban/[id].tsx
@@ -1,31 +1,35 @@
 import AddButton from "@/components/addButton";
-import { Task } from "@/components/task";
 import Head from "next/head";
 import styles from "./../../styles/KanBan.module.css";
+import useIsMobile from "@/hooks/useIsMobile";
+import { Task } from "@/components/task";
 
-export default function kanban() {
+export default function Kanban() {
+  const isMobile = useIsMobile();
+
   return (
-    <div className={styles.kanban}>
-      <Head>
-        <title>Login</title>
-        <meta name="description" content="login of the kan-ban! aplication" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <div className={styles.kanban} style={{ flexDirection: isMobile ? "column" : "row" }}>
+        <Head>
+          <title>Login</title>
+          <meta name="description" content="kan-ban! aplication" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-     <div className={styles.list}>
-        <div className={styles.listHeader}>To do</div>
-        <AddButton/>
-     </div>
+        <div className={styles.list}>
+          <div className={styles.listHeader}>To do</div>
+          <AddButton />
+        </div>
 
-     <div className={styles.list}>
-        <div className={styles.listHeader}>In progress</div>
-     </div>
+        <div className={styles.list}>
+          <div className={styles.listHeader}>In progress</div>
+        </div>
 
-     <div className={styles.list}>
-        <div className={styles.listHeader}>Done</div>
-     </div>
-
-    </div>
+        <div className={styles.list}>
+          <div className={styles.listHeader}>Done</div>
+        </div>
+      </div>
+    </>
   );
 }
