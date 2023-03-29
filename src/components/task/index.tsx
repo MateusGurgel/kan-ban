bb8e788eff460b40ea5a64c5fbd8ef6122263710
Mateus <mateusgurgel15@gmail.com>
2023-03-29 09:20:11 -0300

    Task is draggable now

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -1,14 +1,22 @@
-import styles from "./styles.module.css"
+import { Draggable } from "react-beautiful-dnd";
+import styles from "./styles.module.css";
 
-
-interface TaskProps{
-    text: string;
+interface TaskProps {
+  text: string;
+  index: number;
+  id: string;
 }
 
-export function Task({text} : TaskProps) {
-    return(
-        <div className={styles.task}>
+export function Task({ text, index, id }: TaskProps) {
+  return (
+    <Draggable key={id} draggableId={id} index={index}>
+      {(provided) => ( 
+        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+          <div className={styles.task}>
             <p>{text}</p>
-        </div>
-    )
+          </div>
+        </li>
+      )}
+    </Draggable>
+  );
 }
