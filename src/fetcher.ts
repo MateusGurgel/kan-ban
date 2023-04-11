97782e1aeb30cd73b2e75117ca2cafbc990de2b1
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-04-11 19:59:25 -0300

    Putting a header on the header before making a request

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,6 +1,12 @@
 import { api } from './config';
+import { UserService } from './services/UserService';
 
 const fetcher = async (url: string) => {
+
+  if (UserService.token.get()){
+    UserService.token.addInAuthorizationHeader()
+  }
+
   const response = await api.get(url);
   return response.data;
 };
